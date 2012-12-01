64410943b65bedf69ba5084e29039fe69d2d8ba4
Christian Baker <cbaker@balmain.com.au>
2012-12-01 20:47:04 +1100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home Page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text=> 'Sample App')
     end
-      it "should have the right title" do
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "#{base_title}")
+    end
+      it "should not have home in the title" do
         visit '/static_pages/home'
-        page.should have_selector('title',
-                                  :text => "Ruby on Rails Tutorial Sample App | Home")
+        page.should_not have_selector('title',
+                                  :text => " | Home")
       end
 
   end
@@ -23,7 +31,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
+                                :text => "#{base_title} | Help")
     end
 
   end
@@ -36,9 +44,22 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | About")
+                                :text => "#{base_title} | About")
     end
 
   end
+
+  describe "Contact Page" do
+    it "Should have H1 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text=> 'Contact Us')
+    end
+    it "Should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                :text => "#{base_title} | Contact")
+    end
+  end
+
 
 end
