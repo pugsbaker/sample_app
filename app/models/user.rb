e3869aaa73e501c53c44089d9a0a29d524606d75
Christian Baker <cbaker@balmain.com.au>
2013-02-24 20:12:37 +1100

    Finished signin signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,13 @@ class User < ActiveRecord::Base
 
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
